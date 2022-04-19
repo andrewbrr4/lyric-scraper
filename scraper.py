aeb8c870fb0f3cfe7663b1f3802a0b8b51d58960
Andrew Barry <andrewbarry@MacBook-Pro-4.lan>
2022-04-18 23:46:51 -0400

    need to do some regex cleaning

--- scraper.py
+++ scraper.py
@@ -6,6 +6,11 @@ from lyricsgenius import Genius
 token = config.client_token
 
 genius = Genius(token)
-genius.search_artist('David Bowie')
-artist.save_lyrics()
+songs = genius.search_artist('David Bowie',max_songs = 4).songs
 
+text = ''
+for song in songs:
+    text += (song.lyrics+'\n\n')
+
+with open('output.txt', 'w') as file:  # Use file to refer to the file object
+    file.write(text)
