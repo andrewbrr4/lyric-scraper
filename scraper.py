ba2e413a6c197d80701701685b934f80a04faa45
Andrew Barry <andrewbarry@MacBook-Pro-4.lan>
2022-04-19 19:01:36 -0400

    done, ready to run

--- scraper.py
+++ scraper.py
@@ -2,15 +2,22 @@ import config
 import requests
 import lyricsgenius as lg
 from lyricsgenius import Genius
+import re
 
 token = config.client_token
 
 genius = Genius(token)
-songs = genius.search_artist('David Bowie',max_songs = 4).songs
+songs = genius.search_artist('David Bowie').songs
 
 text = ''
 for song in songs:
-    text += (song.lyrics+'\n\n')
+    lyrics = song.lyrics
+    lyrics = lyrics.lower()
+    lyrics = lyrics.replace('lyrics','')
+    lyrics = lyrics.replace('embed','')
+    lyrics = re.sub(r'\[.*\]','',lyrics)
+    lyrics = re.sub(r'[0-9]','',lyrics)
+    text += (lyrics +'\n\n')
 
 with open('output.txt', 'w') as file:  # Use file to refer to the file object
     file.write(text)
